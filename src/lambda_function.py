29abe59db3a1c5b545e6662b08ae08f8f31d90e4
wendellbigato <wendellbarreto@gmail.com>
2024-05-13 13:19:19 -0300

    primeiro commit

---
+++ src/lambda_function.py
@@ -0,0 +1,9 @@
+import json
+# comentario
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello World from Lambda!')
+    }
+
